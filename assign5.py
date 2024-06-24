7ad80cd18b141431c796c3f0075d2de781992a3d
Swayam Nayak <108256523+Swayam-Nayak05@users.noreply.github.com>
2024-06-24 18:01:09 +0530

    Update assign5.py

--- assign5.py
+++ assign5.py
@@ -39,3 +39,74 @@ def print_rangoli(s):
         print(rev[s-2-i])
 
 # 5)
+def merge_the_tools(string, k):
+    # your code goes here
+     for t in range(0,len(string),k):
+        u = set()
+        for i in range(t,t+k):
+            if string[i] not in u:
+                u.add(string[i])
+                print(string[i],end="")
+        print()
+
+
+# 6)
+from collections import Counter
+X = int(input())
+sizes = list(map(int, input().split()))
+money = 0
+N = int(input())
+for _ in range(N):
+    lst = list(map(int, input().split()))
+    cnt = Counter(sizes)
+    if lst[0] in cnt.keys():
+        money += lst[1]
+        sizes.remove(lst[0])
+    else:
+        money += 0
+print(money)
+
+
+# 7)
+T = int(input())
+for i in range(T):
+    try:
+        a, b = map(int, input().split())
+        print(a//b)
+    except ZeroDivisionError as e:
+        print("Error Code:", e)
+    except ValueError as v:
+        print("Error Code:", v)
+
+
+# 8)
+import re
+n = int(input())
+for _ in range(n):
+    try:
+        s = input()
+        p = re.compile(s)
+        print("True")
+    except re.error:
+        print("False")
+
+
+# 9)
+n = input()
+s = [val for val in input().split(" ")]
+s = s[::-1]
+s = set(s)
+N = int(input())
+
+for _ in range(N):
+    command = input().split(" ")
+    try:
+        if command[0] == "pop":
+            s.pop()
+        elif command[0] == "discard":
+            s.discard(command[1])
+        else:
+            s.remove(command[1])
+    except Exception as e:
+        continue
+print(sum(map(int, s)))  
